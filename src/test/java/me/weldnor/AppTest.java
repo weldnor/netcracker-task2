b220e5e2704c5c45a43525dc00c27cf758c4e8cc
weldnor <kolesov.anton.s@gmail.com>
2021-03-30 00:46:57 +0300

    First project

---
+++ src/test/java/me/weldnor/AppTest.java
@@ -0,0 +1,14 @@
+package me.weldnor;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
+
